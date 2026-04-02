48cde51ae977ad69ac738123a14828eca718a548
Nipun Yasas <bandaraopnyk.22@uom.lk>
2026-04-02 13:47:34 +0530

    initialized springboot project with initial project structure and dependencies

---
+++ src/main/java/com/megaminds/govisaviya/GovisaviyaApplication.java
@@ -0,0 +1,13 @@
+package com.megaminds.govisaviya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovisaviyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovisaviyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/megaminds/govisaviya/GovisaviyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megaminds.govisaviya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovisaviyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
